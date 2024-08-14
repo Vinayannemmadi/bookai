74474a919995059cdf771291ae3cf868ff108d76
Vinaynemmadi <vinaynemmadi7@gmail.com>
2024-08-14 16:39:55 +0530

    mini navbar updated

--- src/components/PricingSection/CodeExamples.jsx
+++ src/components/PricingSection/CodeExamples.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 const CodeExamples = ({ theme }) => {
   return (
     <div
-      id="code-examples"
+      id="api-pricing"
       className={`${
         theme === 'light' ? 'text-black bg-white' : 'text-white bg-[#1e293b]'
-      } p-[24px] m-[28px] rounded-md font-sans 
-      sm:w-full sm:m-2 sm:p-4
+      } p-[32px] m-[32px] rounded-md font-sans 
+      sm:w-full sm:m-0 sm:p-4 
       md:w-[90%] md:m-4 md:p-6
-      lg :w-[85%] lg:m-6 lg:p-8
+      lg:w-[85%] lg:m-6 lg:p-8
       xl:w-[85%] xl:m-12 xl:p-10`}
     >
       <h1 className="text-[24px] mt-[32px] font-bold text-[#a5b4fc] sm:mt-4 sm:text-[20px] md:mt-6 md:text-[22px] lg:mt-8 lg:text-[24px] xl:mt-10 xl:text-[26px]">
--- src/components/PricingSection/PricingSecion.jsx
+++ src/components/PricingSection/PricingSecion.jsx
@@ -7,7 +7,14 @@ import Tutorial from './Tutorial';
 import themeContext from '../store/theme-context';
 import { useContext } from 'react';
 const PricingSection=()=>{
-
+    const navItems = [
+        { path: 'overview', title: 'OVERVIEW' },
+        { path: 'authentication', title: 'AUTHENTICATION' },
+        { path: 'endpoints', title: 'ENDPOINTS' },
+        { path: 'tutorials', title: 'TUTORIALS' },
+        { path: 'code-examples', title: 'CODE EXAMPLES' },
+        { path: 'api-pricing', title: 'PRICING' },
+      ];
     const {theme} = useContext(themeContext);
 
     return (
@@ -16,14 +23,17 @@ const PricingSection=()=>{
                <h1 className='text-[40px] font-sans'> Book Generator API </h1>
             </div>
             
-            <div className='flex justify-center font-sans text-[#E2E8F0] font-sans text-[14px] mb-10 flex-wrap space-x-9 py-[27px] px-7 bg-[#1e293b]'>
-                <a href="/#overview" className="anchor font-bold hover:text-[#22d3ee] ">OVERVIEW</a>
-                <a href="/#authentication" className="anchor font-bold hover:text-[#22d3ee] ">AUTHENTICATION</a>
-                <a href="/#endpoints" className="anchor font-bold hover:text-[#22d3ee] ">ENDPOINTS</a>
-                <a href="/#tutorial" className="anchor font-bold  hover:text-[#22d3ee]">TUTORIAL</a>
-                <a href="/#code-examples" className="anchor font-bold hover:text-[#22d3ee]" >CODE EXAMPLES</a>
-                <a href="/#api-pricing" className="anchor font-bold hover:text-[#22d3ee] ">PRICING</a>
+            <div className='flex flex-col md:flex-row md:flex-wrap justify-center sm:space-x-6 sm:space-y-7  md:space-y-0 md:space-10 
+                sticky top-0 z-1000   font-sans text-[#E2E8F0] text-[14px] mb-10 text-center md:space-7 py-[27px]  bg-[#1e293b] '>
+                <a href="/#overview" className="font-bold hover:text-[#22d3ee]">OVERVIEW</a>
+                <a href="/#authentication" className="font-bold hover:text-[#22d3ee]">AUTHENTICATION</a>
+                <a href="/#endpoints" className="font-bold hover:text-[#22d3ee]">ENDPOINTS</a>
+                <a href="/#tutorial" className="font-bold hover:text-[#22d3ee]">TUTORIAL</a>
+                <a href="/#code-examples" className="font-bold hover:text-[#22d3ee]">CODE EXAMPLES</a>
+                <a href="/#api-pricing" className="font-bold hover:text-[#22d3ee]">PRICING</a>
             </div>
+
+            
             <div className='flex justify-center flex-col items-center mx-auto space-y-10 pb-16 max-w-[1000px]' >
                 <Overview theme={theme}/>
                 <Authentication theme={theme}/>
